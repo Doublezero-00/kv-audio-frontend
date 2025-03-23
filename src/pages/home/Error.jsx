ce740f637f6f1cca2bf4829150bc0aa8f2473f7b
Doublezero-00 <chamothlakshitha97@gmail.com>
2025-03-23 00:32:37 -0700

    Created pages in the Homepage👍

--- src/pages/home/Error.jsx
+++ src/pages/home/Error.jsx
@@ -4,7 +4,7 @@ export default function Error() {
   return (
     <div>
       <h1>404 error : Page not found</h1>
-      <Link to="/" className="bg-red-400 p-1">
+      <Link to="/" className="bg-red-300 p-1">
         Back to homepage
       </Link>
     </div>
